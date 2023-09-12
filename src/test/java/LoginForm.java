3c164912f59c6a40f2253da16e810c893028455f
Maksim Atenyaev <atenjaev@gmail.com>
2023-09-12 22:39:22 +0600

    Исправление замечаний

--- src/test/java/LoginForm.java
+++ src/test/java/LoginForm.java
@@ -3,71 +3,70 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class loginForm {
-
-
+public class LoginForm {
+    String uri = "/automation-practice-form";
     String firstName = "Bruce";
     String lastName = "Wayne";
     String fullName = firstName + " " + lastName;
     String email = "wayneinterprise@waine.com";
+    String gender = "Male";
     String phoneNumber = "7999999999";
     String subject1 = "Physics";
     String subject2 = "Computer Science";
+    String hobbie1 = "Sports";
+    String hobbie2 = "Reading";
+    String hobbie3 = "Music";
     String address = "1007 Mountain Drive, Gotham";
+    String state = "Haryana";
+    String city = "Karnal";
     String summitText = "Thanks for submitting the form";
 
 
-
     @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+    static void BeforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void fillForm() {
-        open("");
+    void FillForm() {
+        open(uri);
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('#app footer').remove()");
-
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $("[for=gender-radio-1]").click();
+        $("#genterWrapper").$(byText(gender)).click();
         $("#userNumber").setValue(phoneNumber);
 
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
-        $("[value='11']").click();
+        $(".react-datepicker").$("[value='11']").click();
         $(".react-datepicker__year-select").click();
-        $("[value='1990']").click();
+        $(".react-datepicker__year-select").$("[value='1990']").click();
         $(".react-datepicker__day--010").click();
-
 
         $("#subjectsInput").setValue(subject1).pressEnter();
         $("#subjectsInput").setValue(subject2).pressEnter();
 
-
-        $("[for='hobbies-checkbox-1']").click();
-        $("[for='hobbies-checkbox-2']").click();
-        $("[for='hobbies-checkbox-3']").click();
+        $("#hobbiesWrapper").$(byText(hobbie1)).click();
+        $("#hobbiesWrapper").$(byText(hobbie2)).click();
+        $("#hobbiesWrapper").$(byText(hobbie3)).click();
 
         $("#uploadPicture").uploadFromClasspath("bruce.jpeg");
-
         $("#currentAddress").setValue(address);
 
         $("#state").click();
-        $("#react-select-3-option-1").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
-        $("#react-select-4-option-1").click();
-
+        $("#stateCity-wrapper").$(byText(city)).click();
         $("#submit").click();
-
 
         $("#example-modal-sizes-title-lg").shouldHave(text(summitText));
         $("tbody").shouldHave(exactText("Student Name " + fullName + "\n" +
@@ -76,27 +75,14 @@ public class loginForm {
                                                     "Mobile " + phoneNumber + "\n" +
                                                     "Date of Birth 10 December,1990" + "\n" +
                                                     "Subjects " + subject1 + ", " + subject2 + "\n" +
-                                                    "Hobbies Sports, Reading, Music\n" +
+                                                    "Hobbies " + hobbie1 + ", " + hobbie2 + ", " + hobbie3 +"\n" +
                                                     "Picture bruce.jpeg" + "\n" +
                                                     "Address " + address + "\n" +
-                                                    "State and City Uttar Pradesh Lucknow"));
+                                                    "State and City " + state + " " + city));
     }
-
-    @Test
-     void closeSummitForm(){
-        $("#closeLargeModal").click();
-        $("body").shouldNotHave(text("Thanks for submitting the form"));
-
-    }
-
 
     @AfterAll
-    static void tearDown(){
+    static void TearDown(){
         Selenide.closeWebDriver();
     }
-
-
-
-
-
 }
