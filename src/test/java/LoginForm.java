36ca5f8aabf6b60f80d5977e260b10e8adc9b7dd
Maksim Atenyaev <atenjaev@gmail.com>
2023-09-18 17:13:30 +0600

    refactor function's names

--- src/test/java/LoginForm.java
+++ src/test/java/LoginForm.java
@@ -27,14 +27,14 @@ public class LoginForm {
 
 
     @BeforeAll
-    static void BeforeAll() {
+    static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void FillForm() {
+    void fillForm() {
         open(uri);
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('#app footer').remove()");
@@ -82,7 +82,7 @@ public class LoginForm {
     }
 
     @AfterAll
-    static void TearDown(){
+    static void tearDown(){
         Selenide.closeWebDriver();
     }
 }
